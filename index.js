2894633a679596c167c7019c19863f5be3803b69
Walterhart <walterwhart1@gmail.com>
2023-06-05 19:33:19 -0400

    Refactor by making parameters for buttons into callback and changing functions name

--- index.js
+++ index.js
@@ -1,38 +1,39 @@
-
+// Variable Declarations
 const imageContainer = document.getElementById("image-cards");
-const newDeckBtn =document.getElementById("new-deck")
-const drawBtn =document.getElementById("draw-cards")
-let deckId
+const newDeckBtn = document.getElementById("new-deck");
+const drawBtn = document.getElementById("draw-cards");
+let deckId;
 
-const getNewDeck = () => {
+const handleDrawCards = () => {
+  fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
+    .then((res) => res.json())
+    .then((data) => {
+      getImage(data);
+    });
+};
+
+const handleNewDeck = () => {
   fetch("https://deckofcardsapi.com/api/deck/new/")
     .then((res) => res.json())
     .then((data) => {
       console.log(data);
       deckId = data.deck_id;
     });
-}
+};
 
-const getImage = (deck)=> {
+const getImage = (deck) => {
   console.log(deck);
   const cardSlotsHTML = deck.cards.map(
     (card) => `
-    <img src="${card.image}" class="card" />
-`
+      <img src="${card.image}" class="card" />
+  `
   );
   const cardSlots = imageContainer.children;
-  
+
   cardSlotsHTML.forEach((html, index) => {
     cardSlots[index].innerHTML = html;
   });
-}
+};
 
-newDeckBtn.addEventListener("click", getNewDeck);
-
-drawBtn.addEventListener("click", () => {
-  fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
-    .then((res) => res.json())
-    .then((data) => {
-      getImage(data);
-    });
-});
+drawBtn.addEventListener("click", handleDrawCards);
+newDeckBtn.addEventListener("click", handleNewDeck);
