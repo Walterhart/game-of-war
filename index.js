6b598f55f9db0c68d835f90901de96c21e24d71f
Walterhart <walterwhart1@gmail.com>
2023-06-05 20:19:18 -0400

    Add display message on card wins

--- index.js
+++ index.js
@@ -2,13 +2,48 @@
 const imageContainer = document.getElementById("image-cards");
 const newDeckBtn = document.getElementById("new-deck");
 const drawBtn = document.getElementById("draw-cards");
+const winnerHTMLText= document.getElementById("winner-message")
 let deckId;
+ 
+const determineWinner = (cpuCard, playerCard) =>{
+    const cardValues = {
+        2: 2,
+        3: 3,
+        4: 4,
+        5: 5,
+        6: 6,
+        7: 7,
+        8: 8,
+        9: 9,
+        10: 10,
+        "JACK": 11,
+        "QUEEN": 12,
+        "KING": 13,
+        "ACE": 14
+    };
+    const cpuCardValue = cardValues[cpuCard]
+    const playerCardValue = cardValues[playerCard]
+    if( cpuCardValue<playerCardValue){
+        return `Computer Wins the Round!`
+    }
+    else if(  cpuCardValue>playerCardValue){
+        return `Player Wins the Round!`
+    }
+    else{
+         return `WAR!`
+    }
+
+}
 
 const handleDrawCards = () => {
   fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
     .then((res) => res.json())
     .then((data) => {
       getImage(data);
+      const cpuCard = data.cards[0].value;
+      const playerCard = data.cards[1].value;
+      const winnerMessage=determineWinner(cpuCard, playerCard);
+      winnerHTMLText.textContent=winnerMessage;
     });
 };
 
