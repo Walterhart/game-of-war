26d0aa875045af81e8514830c2d42af80e9e3a63
Walterhart <walterwhart1@gmail.com>
2023-06-05 22:50:05 -0400

    Add start game over  and disable draw button on load

--- index.js
+++ index.js
@@ -1,73 +1,87 @@
-// Variable Declarations
+// Variable DeclarationssText
 const imageContainer = document.getElementById("image-cards");
 const newDeckBtn = document.getElementById("new-deck");
 const drawBtn = document.getElementById("draw-cards");
-const winnerHTMLText= document.getElementById("winner-message")
-const reamingCardsText = document.getElementById("remaining-cards")
-const computerScoreText =document.getElementById("computer-score")
-const playerScoreText =document.getElementById("player-score")
+const winnerHTMLText = document.getElementById("winner-message");
+const remainingCardsText = document.getElementById("remaining-cards");
+const computerScoreText = document.getElementById("computer-score");
+const playerScoreText = document.getElementById("player-score");
+const restartGameBtn = document.getElementById("restart-game");
 
 let deckId;
 
 let scoreBored = {
-    "Computer": 0,
-    "Player": 0
-  };
-  
- 
-const determineWinner = (cpuCard, playerCard) =>{
-    const cardValues = {
-        2: 2,
-        3: 3,
-        4: 4,
-        5: 5,
-        6: 6,
-        7: 7,
-        8: 8,
-        9: 9,
-        10: 10,
-        "JACK": 11,
-        "QUEEN": 12,
-        "KING": 13,
-        "ACE": 14
-    };
-    const cpuCardValue = cardValues[cpuCard]
-    const playerCardValue = cardValues[playerCard]
-    if( cpuCardValue<playerCardValue){
-        scoreBored["Player"]++;
-        playerScoreText.innerText= `My score: ${scoreBored["Player"]}`
-        return  `Player Wins the Round!`
-    }
-    else if(  cpuCardValue>playerCardValue){
-        scoreBored["Computer"]++;
-        computerScoreText.innerText= `Computer score: ${scoreBored["Computer"]}`
-        return `Computer Wins the Round!`
-    }
-    else{
-         return `WAR!`
-    }
+  Computer: 0,
+  Player: 0,
+};
 
-}
+const handleRestartGame = () => {
+  newDeckBtn.classList.remove("hidden");
+  restartGameBtn.classList.add("hidden");
+  winnerHTMLText.innerText = "Game of War";
+  remainingCardsText.textContent = "";
+  computerScoreText.textContent = "";
+  playerScoreText.textContent = "";
+  scoreBored = {
+    Computer: 0,
+    Player: 0,
+  };
+};
+
+const determineWinner = (cpuCard, playerCard) => {
+  const cardValues = {
+    2: 2,
+    3: 3,
+    4: 4,
+    5: 5,
+    6: 6,
+    7: 7,
+    8: 8,
+    9: 9,
+    10: 10,
+    JACK: 11,
+    QUEEN: 12,
+    KING: 13,
+    ACE: 14,
+  };
+  const cpuCardValue = cardValues[cpuCard];
+  const playerCardValue = cardValues[playerCard];
+  if (cpuCardValue < playerCardValue) {
+    scoreBored["Player"]++;
+    playerScoreText.innerText = `My score: ${scoreBored["Player"]}`;
+    return `Player Wins the Round!`;
+  } else if (cpuCardValue > playerCardValue) {
+    scoreBored["Computer"]++;
+    computerScoreText.innerText = `Computer score: ${scoreBored["Computer"]}`;
+    return `Computer Wins the Round!`;
+  } else {
+    return `WAR!`;
+  }
+};
 
 const handleDrawCards = () => {
+  if (!deckId) {
+    return;
+  }
   fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
     .then((res) => res.json())
     .then((data) => {
       getImage(data);
       const cpuCard = data.cards[0].value;
       const playerCard = data.cards[1].value;
-      const winnerMessage=determineWinner(cpuCard, playerCard);
-      reamingCardsText.textContent =`Remaining Cards:  ${data.remaining}`
-      winnerHTMLText.textContent=winnerMessage;
-      if(data.remaining === 0){
-        drawBtn.disabled = true
-        if (scoreBored["Computer"]< scoreBored["Player"]) {
-          winnerHTMLText.innerText =`Congratulation You Win!`
-        } else if (scoreBored["Computer"]>scoreBored["Player"]) {
-            winnerHTMLText.innerText =`Better Luck Next Time, You Lose!`
+      const winnerMessage = determineWinner(cpuCard, playerCard);
+      remainingCardsText.textContent = `Remaining Cards:  ${data.remaining}`;
+      winnerHTMLText.textContent = winnerMessage;
+      if (data.remaining === 0) {
+        drawBtn.disabled = true;
+        if (scoreBored["Computer"] < scoreBored["Player"]) {
+          winnerHTMLText.innerText = `Congratulation You Win!`;
+        } else if (scoreBored["Computer"] > scoreBored["Player"]) {
+          winnerHTMLText.innerText = `Better Luck Next Time, You Lose!`;
         } else {
-            winnerHTMLText.innerText =`WAR`
+          winnerHTMLText.innerText = `WAR`;
         }
+        restartGameBtn.classList.remove("hidden");
       }
     });
 };
@@ -76,20 +90,20 @@ const handleNewDeck = () => {
   fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
     .then((res) => res.json())
     .then((data) => {
-      reamingCardsText.textContent =`Remaining Cards:  ${data.remaining}`
-      console.log(data);
+      drawBtn.disabled = false;
+      remainingCardsText.textContent = `Remaining Cards:  ${data.remaining}`;
       deckId = data.deck_id;
+      newDeckBtn.classList.add("hidden");
     });
 };
 
 const getImage = (deck) => {
-  console.log(deck);
   const cardSlotsHTML = deck.cards.map(
     (card) => `
       <img src="${card.image}" class="card" />
   `
   );
-  
+
   const cardSlots = imageContainer.children;
 
   cardSlotsHTML.forEach((html, index) => {
@@ -97,5 +111,6 @@ const getImage = (deck) => {
   });
 };
 
+restartGameBtn.addEventListener("click", handleRestartGame);
 drawBtn.addEventListener("click", handleDrawCards);
 newDeckBtn.addEventListener("click", handleNewDeck);
